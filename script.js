e038d6f6e914b423fc64ca167d544b9061d36e7e
rohanofgondor <rohanofgondor@gmail.com>
2026-09-16 22:57:08 +0530

    Step 3 of the RPS from TOP done

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Step 2 (according to TOP)
 function getComputerChoice () {
     let rps = Math.floor(Math.random() * 3) + 1;
+    // Math.random() returns a number from 0 (inclusive) up to but not including 1.
+    // Multiplying by 3 gives a number from 0 up to but not including 3.
+    // Adding 1 shifts that range to 1 up to but not including 11.
+    // Math.floor() then rounds down, so you get an integer between 1 and 3.
     if (rps === 1) {
         console.log ('Rock');
             }
@@ -11,19 +16,9 @@ function getComputerChoice () {
         }
 }
 getComputerChoice();
-
-// Logic
-// First step
-// // Create a function called getComputerChoice and inside of it,
-// Randomly returns Rock Paper Scissor
-// TOP gave hint to say Math.random() method so computer can randomly pick and register one
-
-// Hurdle 1
-//     // I should give it choices to pick randomly from - rock paper scissor
-//     // I could use OR || LO (logical operator) to pick from Rock || Paper || Scissor
-//     // Potential solution - For starter just write, the logic in above sentence.
-//     // Potential solution - **I could assign the 3 multiple choice strings to numbers 1 2 3 = rock
-//     // paper scissors respectively
-//     // then use math.floor and limit the inclusive/exclusive outcomes to integers ranging from
-//     // 0 to 4 (0 i think will be included and 4 will be excluded and floor() also floors the
-// decimals to integers .4 or .5 to 4 5
+// Step 3 (according to TOP)
+function  getHumanChoice () {
+    let userChoice = prompt('Pick one', 'Rock, Paper, Scissors')
+    return (console.log(userChoice));
+}
+getHumanChoice()
